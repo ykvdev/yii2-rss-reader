8f8da15c2f04b91229d697dc185cdc6bb68871b3
atoumus <atoumus@gmail.com>
2016-05-23 07:25:09 +0200

    Register page assets refactor

--- components/Bootstrap.php
+++ components/Bootstrap.php
@@ -3,10 +3,22 @@
 namespace app\components;
 
 use yii\base\BootstrapInterface;
+use yii\base\Event;
+use yii\base\Application;
 
 class Bootstrap implements BootstrapInterface
 {
     public function bootstrap($app) {
+        $this->initActionClasses($app);
+
+        Event::on(
+            \yii\web\Controller::className(),
+            \yii\web\Controller::EVENT_BEFORE_ACTION,
+            [$this, 'registerPageAssets']
+        );
+    }
+
+    public function initActionClasses(Application $app) {
         // Fill $app->controllerMap property for auto run action classes
         $controllersPath = \Yii::getAlias('@app/controllers');
         $dir = new \DirectoryIterator($controllersPath);
@@ -19,6 +31,20 @@ class Bootstrap implements BootstrapInterface
                     ? $namespaceOfDefinedController
                     : Controller::className();
             }
+        }
+    }
+
+    public function registerPageAssets($event) {
+        $assetFileName = str_replace('/', '-', \Yii::$app->controller->route);
+
+        $cssFilePath = '/css/pages/' . $assetFileName . '.css';
+        if(file_exists(\Yii::getAlias('@webroot' . $cssFilePath))) {
+            \Yii::$app->view->registerCssFile('@web' . $cssFilePath);
+        }
+
+        $jsFilePath = '/js/pages/' . $assetFileName . '.js';
+        if(file_exists(\Yii::getAlias('@webroot' . $jsFilePath))) {
+            \Yii::$app->view->registerJsFile('@web' . $jsFilePath);
         }
     }
 }
--- components/Controller.php
+++ components/Controller.php
@@ -24,25 +24,4 @@ class Controller extends \yii\web\Controller
 
         return $actions;
     }
-
-    public function beforeAction($action)
-    {
-        if (!parent::beforeAction($action)) {
-            return false;
-        }
-
-        $assetFileName = str_replace('/', '-', \Yii::$app->controller->route);
-
-        $cssFilePath = '/css/pages/' . $assetFileName . '.css';
-        if(file_exists(\Yii::getAlias('@webroot' . $cssFilePath))) {
-            \Yii::$app->view->registerCssFile('@web' . $cssFilePath);
-        }
-
-        $jsFilePath = '/js/pages/' . $assetFileName . '.js';
-        if(file_exists(\Yii::getAlias('@webroot' . $jsFilePath))) {
-            \Yii::$app->view->registerJsFile('@web' . $jsFilePath);
-        }
-
-        return true;
-    }
 }
